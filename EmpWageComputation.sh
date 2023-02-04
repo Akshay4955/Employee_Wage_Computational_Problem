653b06e60fb96fa7cd393e890f7c62c17975f6e5
Akshay4955 <akshayshedge11@gmail.com>
2023-02-04 13:10:41 +0530

    Added : Employee Monthly Wage Calculation with Daily Wage using array

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,7 +4,8 @@ Monthly_Wage=0;
 Total_Hour=0;
 Day=1;
 function To_Get_Emp_Hour(){
-case $1 in
+present=$((RANDOM%3));
+case $present in
 	1)
 		Emp_Hour=8;
 	;;
@@ -19,7 +20,7 @@ echo $Emp_Hour;
 }
 while [[ $Day -le 20 && $Total_Hour -le 100 ]]
 do
-Emp_Hour=$(To_Get_Emp_Hour $((RANDOM%3)));
+Emp_Hour=$(To_Get_Emp_Hour);
 Total_Hour=$(($Total_Hour+$Emp_Hour));
 	if [ $Total_Hour -eq 100 ]
 	then 
@@ -27,7 +28,9 @@ Total_Hour=$(($Total_Hour+$Emp_Hour));
 		break;
 	fi
 Daily_Wage=$(($Emp_Hour*$WAGE_PER_HOUR));
+Daily_Wage_Array[((count++))]=$Daily_Wage;
 Monthly_Wage=$(($Monthly_Wage+$Daily_Wage));
 ((Day++))
 done
-Echo "Monthly Wage of Employee is equal to $Monthly_Wage";
+echo "Daily Wage of Employee is equal to ${Daily_Wage_Array[@]}"
+echo "Monthly Wage of Employee is equal to $Monthly_Wage";
