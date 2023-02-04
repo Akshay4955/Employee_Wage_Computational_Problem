befe523a4b735d6ff3e07f20205bab5e610e255d
Akshay4955 <akshayshedge11@gmail.com>
2023-02-04 11:44:16 +0530

    Added: Part time Employee to Daily Employee Wage calculation

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,11 +1,14 @@
 #!bin/bash
 WAGE_PER_HOUR=20;
-present=$((RANDOM%2))
+present=$((RANDOM%3))
 if [ $present -eq 0 ]
 then
 	Emp_Hour=0;
-else
+elif [ $present -eq 1 ]
+then
 	Emp_Hour=8;
+else 
+	Emp_Hour=4;
 fi 
 Daily_Wage=$(($Emp_Hour*$WAGE_PER_HOUR));
 echo "Employee Daily Wage is equal to : $Daily_Wage"
