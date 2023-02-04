77b3c01d3e35ef5fa62b8ea3a99d594bd3ad1608
Akshay4955 <akshayshedge11@gmail.com>
2023-02-04 11:58:50 +0530

    Added:Daily Employee Wage calculation using case

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,14 +1,18 @@
 #!bin/bash
 WAGE_PER_HOUR=20;
+IsPresent=1;
+IsPartTime=2;
 present=$((RANDOM%3))
-if [ $present -eq 0 ]
-then
-	Emp_Hour=0;
-elif [ $present -eq 1 ]
-then
-	Emp_Hour=8;
-else 
-	Emp_Hour=4;
-fi 
+case $present in
+	$IsPresent)
+		Emp_Hour=8;
+	;;
+	$IsPartTime)
+		Emp_Hour=4;
+	;;
+	*)
+		Emp_Hour=0;
+	;;
+esac
 Daily_Wage=$(($Emp_Hour*$WAGE_PER_HOUR));
 echo "Employee Daily Wage is equal to : $Daily_Wage"
