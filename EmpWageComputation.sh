6e918083da828e6b5f0dcd475732c95a051c674b
Akshay4955 <akshayshedge11@gmail.com>
2023-02-04 12:28:37 +0530

    Added: Monthly Wage Calculation of Employee for 20 days or 100 Working Hours

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -3,7 +3,9 @@ WAGE_PER_HOUR=20;
 IsPresent=1;
 IsPartTime=2;
 Monthly_Wage=0;
-for ((Day=1; Day<=20;Day++))
+Total_Hour=0;
+Day=1;
+while [[ $Day -le 20 && $Total_Hour -le 100 ]]
 do
 present=$((RANDOM%3))
 case $present in
@@ -17,7 +19,14 @@ case $present in
 		Emp_Hour=0;
 	;;
 esac
+Total_Hour=$(($Total_Hour+Emp_Hour));
+	if [ $Total_Hour -eq 100 ]
+	then 
+		Total_Hour=$(($Total_Hour -$Emp_hour));
+		break;
+	fi
 Daily_Wage=$(($Emp_Hour*$WAGE_PER_HOUR));
 Monthly_Wage=$(($Monthly_Wage+$Daily_Wage));
+((Day++))
 done
 Echo "Monthly Wage of Employee is equal to $Monthly_Wage";
