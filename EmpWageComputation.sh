d8dcdbf6cc1c9738678fdba0b8c46067d131b602
Akshay4955 <akshayshedge11@gmail.com>
2023-02-04 13:37:11 +0530

    Added: Emp Monthly Wage Calculation with Day and Daily Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -24,13 +24,14 @@ Emp_Hour=$(To_Get_Emp_Hour);
 Total_Hour=$(($Total_Hour+$Emp_Hour));
 	if [ $Total_Hour -eq 100 ]
 	then 
-		Total_Hour=$(($Total_Hour -$Emp_hour));
+		Total_Hour=$(($Total_Hour - $Emp_hour));
 		break;
 	fi
 Daily_Wage=$(($Emp_Hour*$WAGE_PER_HOUR));
-Daily_Wage_Array[((count++))]=$Daily_Wage;
+Daily_Wage_Dict["$Day"]=$Daily_Wage;
 Monthly_Wage=$(($Monthly_Wage+$Daily_Wage));
 ((Day++))
 done
-echo "Daily Wage of Employee is equal to ${Daily_Wage_Array[@]}"
+echo "Day of Daily Wage is equal to ${!Daily_Wage_Dict[@]}"
+echo "Daily Wage of Employee is equal to ${Daily_Wage_Dict[@]}"
 echo "Monthly Wage of Employee is equal to $Monthly_Wage";
